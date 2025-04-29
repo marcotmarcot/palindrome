77c6488a28f88fe79cab7860b65c8834a639a341
Marco Gontijo <marcotmarcot@gmail.com>
2025-04-29 10:32:04 +0100

    names

--- trie.cc
+++ trie.cc
@@ -21,12 +21,12 @@ public:
   std::map<char, Trie> next_;
 };
 
-bool TryPalindrome(const Trie& root, const Trie& forward, const std::string& context, const std::string& infix, std::string::const_reverse_iterator begin, std::string::const_reverse_iterator end) {
+bool TryPalindrome(const Trie& root, const Trie& current, const std::string& context, const std::string& infix, std::string::const_reverse_iterator begin, std::string::const_reverse_iterator end) {
   while (begin != end && *begin == ' ') {
     ++begin;
   }
   if (begin == end) {
-    if (!forward.final_) {
+    if (!current.final_) {
       return false;
     }
     std::string reversed(context);
@@ -34,35 +34,35 @@ bool TryPalindrome(const Trie& root, const Trie& forward, const std::string& con
     std::cout << context << infix << reversed << std::endl;
     return true;
   }
-  auto it = forward.next_.find(*begin);
-  return it != forward.next_.end() && TryPalindrome(root, it->second, context, infix, begin + 1, end) || forward.final_ && infix.empty() && TryPalindrome(root, root, context, infix, begin, end);
+  auto it = current.next_.find(*begin);
+  return it != current.next_.end() && TryPalindrome(root, it->second, context, infix, begin + 1, end) || current.final_ && infix.empty() && TryPalindrome(root, root, context, infix, begin, end);
 }
 
 int main() {
-  Trie trie;
+  Trie root;
   std::ifstream words("/home/marcots/Downloads/br-sem-acentos.txt");
   std::string word;
   while (words >> word) {
-    trie.Add(word.cbegin(), word.cend());
+    root.Add(word.cbegin(), word.cend());
   }
 
   std::string context;
-  std::deque<std::pair<std::string, const Trie*>> todo{{"", &trie}};
+  std::deque<std::pair<std::string, const Trie*>> todo{{"", &root}};
   while (!todo.empty()) {
     auto [context, current] = todo.front();
     todo.pop_front();
-    TryPalindrome(trie, *current, context, "", context.crbegin(), context.crend());
-    for (const auto& fi : current->next_) {
+    TryPalindrome(root, *current, context, "", context.crbegin(), context.crend());
+    for (const auto& it : current->next_) {
       if (!context.empty() && context.back() != ' ') {
-	TryPalindrome(trie, fi.second, context, {fi.first}, context.crbegin(), context.crend());
+	TryPalindrome(root, it.second, context, {it.first}, context.crbegin(), context.crend());
       }
-      context.push_back(fi.first);
-      todo.push_back({context, &(fi.second)});
+      context.push_back(it.first);
+      todo.push_back({context, &(it.second)});
       context.pop_back();
     }
     if (current->final_) {
       context.push_back(' ');
-      todo.push_back({context, &trie});
+      todo.push_back({context, &root});
       context.pop_back();
     }
   }
